dc63d25c8f4e579cd938b97550ed8c1d40959708
ysn1337 <yassine.chouikh.61@edu.uiz.ac.ma>
2025-05-21 21:54:38 +0100

    Fix syntax

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-return "Hello, DevOps World!"
+    return "Hello, DevOps World!"
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
